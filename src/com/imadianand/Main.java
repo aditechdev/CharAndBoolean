d677710dcad44eefd1be501c93d31c075427dca3
dev adi <aditya.anand156@gmail.com>
2018-04-08 17:32:44 +0530

    Initial commit

---
+++ src/com/imadianand/Main.java
@@ -0,0 +1,13 @@
+package com.imadianand;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//find the code for registered symbol
+        //creat a variable of type char and assign the unicode value
+        //display in on screen
+        char myRegistered=
+            '\u00AE';
+        System.out.println("Regestiration Symbol= " +myRegistered);
+    }
+}
